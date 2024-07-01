b23bdb8e39c97267bd88f51923bab389b577e9d4
Shushant-Priyadarshi <profshushant@gmail.com>
2024-07-02 00:53:58 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-extrabold">Blog App</h1>
+      <h1 className="text-4xl font-extrabold">Under Making</h1>
     </main>
   );
 }
